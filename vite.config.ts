10a96cc92759eb7081dd2eee8d3371ea36e788a1
ayuwidyaagata <ayuwagata@gmail.com>
2026-08-14 17:54:52 +0700

    chore: add Nitro for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     react(),
     tailwindcss(),
+    nitro({
+      preset: "vercel",
+    }),
   ],
 
   resolve: {
